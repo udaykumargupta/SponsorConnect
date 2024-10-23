c7f48cbfcf21fc16b9f08b85999f755b8d5a0384
udaykumargupta <udaykumargupta1104@gmail.com>
2024-10-23 18:50:42 +0530

    initializing project ,connecting with github

---
+++ src/main/java/com/uday/social/media/platform/config.java
@@ -0,0 +1,4 @@
+package com.uday.social.media.platform;
+
+public class config {
+}
---
+++ src/test/java/com/uday/social/media/platform/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uday.social.media.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
