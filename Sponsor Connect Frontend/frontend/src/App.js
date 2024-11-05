0566a8ba9c68daa39811d18f91af31ab88f1dbcc
udaykumargupta <udaykumargupta1104@gmail.com>
2024-11-05 13:44:47 +0530

    Implement custom CSS for navigation bar

--- Sponsor Connect Frontend/frontend/src/App.js
+++ Sponsor Connect Frontend/frontend/src/App.js
@@ -1,10 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Authentication from './Components/Authentication/Authentication';
+import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="">
-
+      <Routes>
+        <Route path="/" element={true?<HomePage></HomePage>:<Authentication></Authentication>}>
+          
+        </Route>
+      </Routes>
     </div>
   );
 }
